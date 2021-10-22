d8ab53e8cd89c9b51ee0a683e687ee11b041b574
Cristhian Torres <cristhian.torres@mail.escuelaing.edu.co>
2021-10-22 13:15:18 -0500

    pom y procfile

---
+++ src/main/java/edu/eci/arsw/Api/EcirilloApplication.java
@@ -0,0 +1,17 @@
+package edu.eci.arsw.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class EcirilloApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(EcirilloApplication.class, args);
+    }
+}
