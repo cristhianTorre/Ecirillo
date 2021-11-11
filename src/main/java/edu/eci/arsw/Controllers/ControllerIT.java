7c333ce62e32f454519dd9dcf0e7733dfb14a984
Cristhian Torres <cristhian.torres@mail.escuelaing.edu.co>
2021-11-11 09:54:06 -0500

    arreglo del pom

--- src/main/java/edu/eci/arsw/Controllers/ControllerIT.java
+++ src/main/java/edu/eci/arsw/Controllers/ControllerIT.java
@@ -2,15 +2,14 @@ package edu.eci.arsw.Controllers;
 
 import javafx.beans.binding.BooleanExpression;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
-import org.testng.annotations.Test;
+//import org.testng.annotations.Test;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ControllerIT {
-    @Autowired
-    private TestRestTemplate template;
+    //@Autowired
     /**
     @Test
     public void getHello() throws Exception {
