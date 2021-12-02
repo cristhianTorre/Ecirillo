21e8809a46878f4233a151298d629a8a4ba03fd9
Jonathan Pulido <jonathan.pulido@mail.escuelaing.edu.co>
2021-12-02 16:54:37 -0500

    basic stomp broker

--- src/main/java/edu/eci/arsw/Controllers/EcirilloController.java
+++ src/main/java/edu/eci/arsw/Controllers/EcirilloController.java
@@ -1,9 +1,13 @@
 package edu.eci.arsw.Controllers;
 
+import edu.eci.arsw.Model.Message;
 import edu.eci.arsw.Services.EcirilloServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +19,13 @@ public class EcirilloController {
 
     @Autowired
     EcirilloServices es= null;
+    @Autowired
+    SimpMessagingTemplate msgt;
+
+    @MessageMapping("/Ecirillo.{room}")
+    public synchronized void handlePointEvent(Message message, @DestinationVariable String room) throws Exception{
+        msgt.convertAndSend("/topic."+room, message);
+    }
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<?> GetAllBlueprintFilter(){
